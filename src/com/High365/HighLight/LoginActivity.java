ce2bc85ef9076db1b20fe72469770513ffb76a94
hupeng <hupeng@imudges.com>
2016-01-25 14:42:59 +0800

    update

--- src/com/High365/HighLight/LoginActivity.java
+++ src/com/High365/HighLight/LoginActivity.java
@@ -3,6 +3,8 @@ package com.High365.HighLight;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,7 +31,13 @@ public class LoginActivity extends Activity {
     /**
      * 执行用户登录时的业务逻辑所在的类
      * */
-    private UserInfoService userInfoService = new UserInfoService();
+    private UserInfoService userInfoService;
+
+    /**
+     * 定义常量
+     * */
+    final private int SUCCESS=1;
+    final private int FAILURE=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,25 +48,15 @@ public class LoginActivity extends Activity {
         passwordTextView=(EditText)findViewById(R.id.passwordText);
         loginButton=(Button)findViewById(R.id.LoginButton);
 
-        inti();
+        init();
 
-        userInfoService.login(usernameTextView.getText().toString(), passwordTextView.getText().toString(), new Listener() {
-            @Override
-            public void onSuccess() {
-                Toast.makeText(LoginActivity.this,"登录成功",Toast.LENGTH_SHORT).show();
-            }
 
-            @Override
-            public void onFailure(String msg) {
-                Toast.makeText(LoginActivity.this,"登录成功",Toast.LENGTH_SHORT).show();
-            }
-        }, this);
     }
 
     /**
      * 初始化
      */
-    private void inti(){
+    private void init(){
         loginButton.setOnClickListener(new View.OnClickListener() {
             /**
              * 给登陆按钮绑定监听函数，用于处理登陆请求
@@ -66,13 +64,62 @@ public class LoginActivity extends Activity {
              */
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MyActivity.class);
-                LoginActivity.this.startActivity(intent);
-                LoginActivity.this.finish();
+                loginButton.setEnabled(false);
+                userInfoService = new UserInfoService();
+                userInfoService.login(usernameTextView.getText().toString(), passwordTextView.getText().toString(), new Listener() {
+                    @Override
+                    public void onSuccess() {
+                        Message message = new Message();
+                        message.what = 1;
+                        message.obj = "登录成功";
+                        handler.sendMessage(message);
+                    }
+
+                    @Override
+                    public void onFailure(String msg) {
+                        Message message = new Message();
+                        message.what = 0;
+                        message.obj = "登录失败,失败原因:" + msg;
+
+                        handler.sendMessage(message);
+                    }
+                }, LoginActivity.this);
+
             }
         });
     }
 
+    /**
+     * 线程间的通信,接受不同的消息请求，做出处理,因为网络请求在子线程中完成,而要在主线程UI上显示网络请求的结果必须要经过线程间通信
+     */
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
 
+            switch (msg.what) {
+                case SUCCESS:
+                    onSuccess();
+                    break;
+                case FAILURE:
+                    String msgStr = (String) msg.obj;
+                    onFailure(msgStr);
+                    break;
+                default:
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
 
+    void onSuccess(){
+        Toast.makeText(LoginActivity.this,"登录成功",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(LoginActivity.this, MyActivity.class);
+        LoginActivity.this.startActivity(intent);
+        LoginActivity.this.finish();
+    }
+
+    void onFailure(String msg){
+        Toast.makeText(LoginActivity.this,msg,Toast.LENGTH_SHORT).show();
+        loginButton.setEnabled(true);
+    }
 }
--- src/com/High365/HighLight/UserInfoService.java
+++ src/com/High365/HighLight/UserInfoService.java
@@ -3,15 +3,27 @@ package com.High365.HighLight;
 import android.content.Context;
 import android.content.SharedPreferences;
 import com.google.gson.Gson;
+import android.os.Looper;
 
 /**
  * @author hupeng
  * 这个类主要负责UserInfo的业务逻辑的处理和本地数据库的访问
+ *
+ * 此类继承于线程类
+ * url:服务器的url地址的一部分,IP地址与端口号在HttpRequest类中定义,这样做只需要修改那边的HOST的值就可以方便的更换服务器的地址与参数
+ * param:参数
+ * httpResponseOutputStreamString:服务器所返回的json字符串
+ * 由于Android的网络通信模块必须放在子线程中,若放在主线程中会导致阻塞主线程.
  */
-public class UserInfoService {
+public class UserInfoService extends Thread{
 
 
-
+    String url;
+    String param;
+    String httpResponseOutputStreamString;
+    Listener listener;
+    Integer taskId;
+    Context context;
 
     /**
      * @param username 用户名
@@ -19,63 +31,49 @@ public class UserInfoService {
      * 成功回调:onSuccess,失败回调:onFailure.
      * */
     public void login(String username,String password,Listener listener,Context context){
-        String url = null;
-        String param = null;
-        String httpRespoenseString = null;
-
+        this.listener = listener;
 
         url = "login.action";
         param = "username=" + username + "&password=" + password;
+        taskId = 0;
+        this.context = context;
+        start();
 
-        MyThread myThread = new MyThread(url,param);
-
-        try{
-            myThread.start();
-            httpRespoenseString = myThread.getHttpResponseOutputStreamString();
-            LoginModel loginModel = new Gson().fromJson(httpRespoenseString,LoginModel.class);
-            if (loginModel.getStatus().equals(1)){
-                SharedPreferences sharedPreferences = context.getSharedPreferences("config",context.MODE_MULTI_PROCESS);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString("secretKey",loginModel.getSecretKey());
-                editor.commit();
-                listener.onSuccess();
-                return;
-            }else {
-                listener.onFailure(loginModel.getErrorInfo());
-                return;
-            }
-        }catch (Exception e){
-            listener.onFailure("网络连接失败");
-            return;
-        }
     }
 
-    /**
-     * 此类为一个线程类
-     * url:服务器的url地址的一部分,IP地址与端口号在HttpRequest类中定义,这样做只需要修改那边的HOST的值就可以方便的更换服务器的地址与参数
-     * param:参数
-     * httpResponseOutputStreamString:服务器所返回的json字符串
-     * 由于Android的网络通信模块必须放在子线程中,若放在主线程中会导致阻塞主线程.
-     * */
-    class MyThread extends Thread{
-        String url;
-        String param;
-        String httpResponseOutputStreamString;
 
-        public MyThread(String url,String param){
-            this.url = url;
-            this.param = param;
+
+
+
+
+    @Override
+    public void run() {
+        Looper.prepare();
+        switch (taskId){
+            case 0:
+                try{
+                    httpResponseOutputStreamString = HttpRequest.sendPost(url,param);
+                    LoginModel loginModel = new Gson().fromJson(httpResponseOutputStreamString,LoginModel.class);
+                    if (loginModel.getStatus().equals(1)){
+                        SharedPreferences sharedPreferences = context.getSharedPreferences("config",context.MODE_MULTI_PROCESS);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("secretKey",loginModel.getSecretKey());
+                        editor.commit();
+                        listener.onSuccess();
+                        return;
+                    }else {
+                        listener.onFailure(loginModel.getErrorInfo());
+                        return;
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                    listener.onFailure("网络连接失败");
+                    return;
+                }
+            default:
+                break;
         }
-
-        @Override
-        public void run() {
-            httpResponseOutputStreamString = HttpRequest.sendPost(url,param);
-        }
-
-
-        public String getHttpResponseOutputStreamString() {
-            return httpResponseOutputStreamString;
-        }
+        stop();
     }
 
     /**
@@ -115,5 +113,4 @@ public class UserInfoService {
         private String secretKey;
         private String errorInfo;
     }
-
 }
