45a50b31e189dcafd64f271ff073b4d27a5fbf74
shianqi <shianqi@imudges.com>
2016-02-01 23:39:35 +0800

    适配器完成，样式调整

--- src/com/High365/HighLight/PageTwo.java
+++ src/com/High365/HighLight/PageTwo.java
@@ -7,7 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 
 /**
@@ -18,6 +23,8 @@ public class PageTwo extends Fragment{
 
     private ListView list;
     private List<LoveLogBean> listItem;
+    private SimpleAdapter simpleAdapter;
+    private ArrayList<HashMap<String,Object>> itemArrayList;
     private SharedPreferencesManager sharedPreferencesManager;
     private String UserID;
     private SqlLiteManager sqlLiteManager;
@@ -32,7 +39,6 @@ public class PageTwo extends Fragment{
         super.onStart();
 
         init();
-        listItem = getListItem();
     }
 
     /**
@@ -47,12 +53,42 @@ public class PageTwo extends Fragment{
     /**
      * 初始化过程，负责类的实例化和组件的绑定
      */
+
+    /**
+     * 获得每一个Item的信息
+     */
+    public void getItemInformation(){
+        HashMap<String,Object> map = new HashMap<String,Object>();
+        map.put("TIME","2016-01-10");
+        for(int i=0;i<10;i++){
+            itemArrayList.add(map);
+        }
+
+    }
+
     public void init(){
+        itemArrayList = new ArrayList<HashMap<String, Object>>();
         sharedPreferencesManager=new SharedPreferencesManager(getActivity());
         sqlLiteManager = new SqlLiteManager(getActivity());
         list = (ListView)getActivity().findViewById(R.id.ListView);
+        /**
+         * 适配器
+         */
+        simpleAdapter = new SimpleAdapter(getActivity(),
+                itemArrayList,
+                R.layout.list_items,
+                new String[]{"TIME"},
+                new int[]{R.id.list_item_title});
+
+        listItem = getListItem();
+        getItemInformation();
+        list.setAdapter(simpleAdapter);
     }
 
+    /**
+     * 获取当前登陆用户的声音数据信息
+     * @return 一个声音数据的List
+     */
     public List<LoveLogBean> getListItem(){
         UserID = sharedPreferencesManager.readString("UserID");
         Log.i("UserID:", ""+UserID);
