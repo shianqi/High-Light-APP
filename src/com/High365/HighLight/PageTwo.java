a6f6eb3cf7419a7a4a118b83561e432ebc1da566
shianqi <shianqi@imudges.com>
2016-02-02 15:10:43 +0800

    ItemDialog编写

--- src/com/High365/HighLight/PageTwo.java
+++ src/com/High365/HighLight/PageTwo.java
@@ -1,13 +1,17 @@
 package com.High365.HighLight;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -50,9 +54,6 @@ public class PageTwo extends Fragment{
             Log.i("音频数据有：",""+listItem.get(i).getSexFrameState());
         }
     }
-    /**
-     * 初始化过程，负责类的实例化和组件的绑定
-     */
 
     /**
      * 获得每一个Item的信息
@@ -66,6 +67,9 @@ public class PageTwo extends Fragment{
 
     }
 
+    /**
+     * 初始化过程，负责类的实例化和组件的绑定
+     */
     public void init(){
         itemArrayList = new ArrayList<HashMap<String, Object>>();
         sharedPreferencesManager=new SharedPreferencesManager(getActivity());
@@ -83,6 +87,39 @@ public class PageTwo extends Fragment{
         listItem = getListItem();
         getItemInformation();
         list.setAdapter(simpleAdapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ToastManager.toast(getActivity(),"点击第:"+position+"个");
+            }
+        });
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("操作")
+                        .setNegativeButton("取消", new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setNeutralButton("删除", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setPositiveButton("分享", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .show();
+                return false;
+            }
+        });
     }
 
     /**
