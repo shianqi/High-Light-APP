4c251583cbf1cc4daba4484024be8bd2040b8717
hupeng <hupeng@imudges.com>
2016-02-04 17:55:20 +0800

    update

---
+++ src/com/High365/HighLight/RecordService.java
@@ -0,0 +1,24 @@
+package com.High365.HighLight;
+
+/**
+ * 录音处理类,向下连接 AudioRecorder.class,向上为UI层提供接口
+ */
+public class RecordService {
+    private short buffer[] = new short[10000];
+
+
+    /**
+     * 计算客观分数,参数与逻辑需要补全
+     * */
+    public int getSexObjectiveScore(){
+        return 0;
+    }
+
+    /**
+     * 计算性的状态帧
+     * */
+
+    public String getSexFrameState(){
+        return null;
+    }
+}
