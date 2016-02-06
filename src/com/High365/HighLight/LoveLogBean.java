f62f948b4e0c8504b50a763f64ddb3a3baf8e235
hupeng <hupeng@imudges.com>
2016-02-06 12:17:53 +0800

    补充了一些JavaDoc

--- src/com/High365/HighLight/LoveLogBean.java
+++ src/com/High365/HighLight/LoveLogBean.java
@@ -10,16 +10,49 @@ import java.sql.Timestamp;
  * 本JavaBean对应本地数据库的LovaLog表
  */
 public class LoveLogBean {
+    /**
+     * 日志ID
+     * */
     private Integer logID;
+    /**
+     * 用户名,用户的唯一标识
+     * */
     private String userId;
+    /**
+     * 性的开始时间
+     * */
     private Timestamp sexStartTime;
+    /**
+     * 性的结束时间
+     * */
     private Timestamp sexEndTime;
+    /**
+     * 性的持续时间
+     * */
     private Timestamp sexTime;
+    /**
+     * 性高潮时间
+     * */
     private Timestamp sexHighTime;
+    /**
+     * 主观评分,由用户给出
+     * */
     private Integer sexSubjectiveScore;
+    /**
+     * 客观评分,由计算所得
+     * */
     private Integer sexObjectiveScore;
+    /**
+     * 性的状态帧
+     * */
     private String sexFrameState;
+    /**
+     * 保存在本地的录音文件的文件名
+     * */
     private String recordFileName;
+    /**
+     * 上传成功标识,未上传成功值为0,上传成功以后为非零值
+     * */
     private Integer updateFlag;
 
     public Integer getLogID() {
